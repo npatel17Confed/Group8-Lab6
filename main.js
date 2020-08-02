d0a5da5f88c0860d3180390d2ad6907c0c984fab
Chintan Trivedi <chintantrivedi247@gmail.com>
2020-08-02 16:53:33 -0400

    changed menubar style and added javascript code

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+var btn = document.getElementById("button");
+var nav_bar = document.getElementById("nav-bar");
+
 document.getElementById("submit").addEventListener("click", function(e) {
 	e.preventDefault();
 	var error = document.getElementById("error");
         var re = /\S+@\S+\.\S+/;
-	var email = document.getElementById("email").value;
+	var email = document.getElementById("email").innerHTML;
 	if(re.test(email)) {
 		document.getElementById("error").innerHTML = "<p id='green'>You were successfully added to the newsletter group.</p>";
 	} else {
@@ -10,6 +13,24 @@ document.getElementById("submit").addEventListener("click", function(e) {
 	}
 	document.getElementById("form").reset();
 });
+
+btn.addEventListener("click", function(e){
+  /* function called when you click of the button */
+      /* this if else to change the text in the button */
+      if(escape(btn.innerHTML) == "%u2630"){
+       	btn.innerHTML="&times;";
+       	nav_bar.style.visibility = "visible";
+       	nav_bar.style.height = "100vh";
+      } else {
+        btn.innerHTML="&#x2630;";
+        nav_bar.style.visibility = "hidden";
+       	nav_bar.style.height = "0";
+      }
+    
+    /* this function toggle the visibility of our "li" elements */
+    
+});
+
 
 
     
